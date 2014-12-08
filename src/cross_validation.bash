4b557265615fd1d00e1e07ebd3025077a43525ad
Alexis Semple <alexis.semple@epfl.ch>
2014-12-08 15:20:21 +0000

    cross_valid with error retainer

--- src/cross_validation.bash
+++ src/cross_validation.bash
@@ -2,7 +2,7 @@
 clear
 
 #Trains and tests on folders for cross-validation
-#output result_basic_NB.txt with
+#output result_stem_NB.txt with
 #1st line count of successful classifications
 #2nd line count of total classifications
 #-Run in parent folder of cross-validation subfolders
@@ -13,27 +13,29 @@ do
 
   echo "--------Creating training data for train$i--------"
   echo
-  java filter_train train"$i"/train
+  java -cp '.:org.tartarus.snowball.jar' filter_train train"$i"/train
   echo "--------Testing files for train$i--------"
   echo
   count=0
+  mkdir train"$i"/misclassified
   for file in train"$i"/test/*
   do
     echo "file = $file"
     ((total++))
-    java filterb "$file" > train"$i"/result_basic_NB.txt
-    res=`cat train$i/result_basic_NB.txt`
+    java filterb "$file" > train"$i"/result_stem_NB.txt
+    res=`cat train$i/result_stem_NB.txt`
     echo "result = $res"
 
     if [[ "$file" =~ .*"$res".* ]]; then
       ((count++))
+    else cp "$file" train"$i"/misclassified
     fi
     echo "count = $count"
     echo "total = $total"
   done
 
-  echo "$count" > train"$i"/result_basic_NB.txt
-  echo "$total" >> train"$i"/result_basic_NB.txt
+  echo "$count" > train"$i"/result_stem_NB.txt
+  echo "$total" >> train"$i"/result_stem_NB.txt
 
   mv training_data.txt train"$i"
 
--- src/filter_train.java
+++ src/filter_train.java
@@ -13,6 +13,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Scanner;
+import org.tartarus.snowball.EnglishSnowballStemmerFactory;
+import org.tartarus.snowball.util.StemmerException;
 
 /**
  * @author Prisca Aeby, Alexis Semple
@@ -21,38 +23,38 @@ import java.util.Scanner;
  * and stores it in a new text file.
  */
 public class filter_train {
-	
+
 	/**
-	 * Set of all distinct words occurring in all e-mails. 
-	 * First entry of the table is for the count of the word occurring in all spam files, 
+	 * Set of all distinct words occurring in all e-mails.
+	 * First entry of the table is for the count of the word occurring in all spam files,
 	 * the second is the same but for ham files,
-	 * the third contains the conditional probability of the word knowing pSpam (P(word|pSpam)), 
+	 * the third contains the conditional probability of the word knowing pSpam (P(word|pSpam)),
 	 * the fourth conditional probability of the word knowing pHam (P(word|pHam)).
 	 */
 	private static HashMap<String, double[]> vocabulary = new HashMap<String, double[]>();
 
 	/**
-	 * The total number of words 
+	 * The total number of words
 	 * totalWords[0]: in all the spam files
-	 * totalWords[1]: in all the ham files 
+	 * totalWords[1]: in all the ham files
 	 */
 	private static int[] totalWords = new int[2];
 
 	/**
 	 * The main function for the program
 	 * @param args The expected structure is: first, the path to the directory of training data, second the file to be classified
+	 * @throws StemmerException
 	 */
-	public static void main (String[] args) {
-		
+	public static void main (String[] args) throws StemmerException {
+
 		if(args.length != 1) {
 			throw new IllegalArgumentException("Wrong number of arguments\n" +
 					"The proper usage is: java filter_train training_directory");
 		}
 
-		//directory containing ham and spam 
-		//TODO replace with own path to directory containing training data.
+		//directory containing ham and spam
 		File directory = new File(args[0]);
-		
+
 		//filter selects only ham and spam files in the directory
 		FileFilter filter = new FileFilter(){
 			public boolean accept(File file){
@@ -83,29 +85,32 @@ public class filter_train {
 		wordCounter(spamFiles, 0);
 		wordCounter(hamFiles, 1);
 
-		//Calculating conditional probabilities 
+		//TODO Uncomment following line to use stop-words preprocessing
+		//stopWords();
+
+		//Calculating conditional probabilities
 		for(String word:vocabulary.keySet()){
 			double[] tab = vocabulary.get(word);
 			tab[2] = Math.log((tab[0]+1)/((double)(totalWords[0]+vocabulary.size())));
 			tab[3] = Math.log((tab[1]+1)/((double)(totalWords[1]+vocabulary.size())));
 			vocabulary.put(word, tab);
 		}
-		
+
 		try {
 			toFile(pSpam, pHam);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		stopWords();
+
 	}
 
 	/**
 	 * Function to count all instances of each word occurring in the list of files
 	 * @param list Contains all files with words to be counted
 	 * @param i indicates whether files in list are spam (i = 0) or ham (i = 1)
+	 * @throws StemmerException
 	 */
-	private static void wordCounter(List<File> list, int i){
+	private static void wordCounter(List<File> list, int i) throws StemmerException{
 
 		Scanner scanner;
 		for(File f:list){
@@ -114,6 +119,8 @@ public class filter_train {
 				while(scanner.hasNext()){
 					String word = scanner.next();
 					totalWords[i]++;
+					//TODO Uncomment following line to enable stemming preprocessing
+					word = EnglishSnowballStemmerFactory.getInstance().process(word);
 					if(vocabulary.containsKey(word)){
 						double[] tab = vocabulary.get(word);
 						tab[i]++;
@@ -130,7 +137,7 @@ public class filter_train {
 			}
 		}
 	}
-	
+
 	/**
 	 * Writes the training results to file "training_data.txt"
 	 * The file is structured as follows:
@@ -142,22 +149,22 @@ public class filter_train {
 	 * @throws IOException
 	 */
 	private static void toFile(double pSpam, double pHam) throws IOException {
-		
+
 		FileWriter fstream = new FileWriter("training_data.txt");
 		BufferedWriter out = new BufferedWriter(fstream);
-		
+
 		Iterator<Entry<String, double[]>> iterator = vocabulary.entrySet().iterator();
-		
+
 		out.write(pSpam + " " + pHam + "\n");
-		
+
 		while(iterator.hasNext()) {
 			Map.Entry<String, double[]> entry = iterator.next();
 			out.write(entry.getKey() + " " + doubleToString(entry.getValue()) + "\n");
 		}
 		out.close();
 	}
-	
-	
+
+
 	/**
 	 * Returns a string of all values separated by a space in an array of doubles.
 	 * @param array of doubles
@@ -170,12 +177,12 @@ public class filter_train {
 		}
 		return s;
 	}
-	
+
 	private static void stopWords(){
         ArrayList<String> stopWords = new ArrayList<String>(Arrays.asList("a", "an", "and", "are", "as", "at", "be", "but", "by",
                                 "for", "if", "in", "into", "is", "it", "no", "not", "of", "on", "or", "such", "that", "the", "their", "then", "there",
                                 "these", "they", "this", "to", "was", "will", "with"));
-        for(String word:vocabulary.keySet()){
+        for(String word: vocabulary.keySet()){
                 if(stopWords.contains(word)){
                         vocabulary.remove(word);
                 }
